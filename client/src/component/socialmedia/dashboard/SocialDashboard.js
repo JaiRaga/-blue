f0c01c8a778d110d6886a368a34d5f679efe93f1
JaiRaga <raga.j.santhosh@gmail.com>
2020-11-27 15:03:21 +0530

    added posts component and responsive breakpoints

--- client/src/component/socialmedia/dashboard/SocialDashboard.js
+++ client/src/component/socialmedia/dashboard/SocialDashboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Grid, makeStyles, Hidden } from "@material-ui/core";
+import SideNav from "../layout/SideNav";
+import Posts from "../social/Posts";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,9 +11,12 @@ const useStyles = makeStyles((theme) => ({
   container: {
     minHeight: "95vh"
   },
-  children: {
+  post: {
     background: "#666"
     // minHeight: "100%"
+  },
+  posts: {
+    background: "#985"
   }
 }));
 
@@ -19,14 +24,24 @@ const SocialDashboard = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Grid container className={classes.container} alignItems='stretch'>
+      <Grid container className={classes.container}>
         <Hidden smDown>
           <Grid item md={3} className={classes.sidenav}>
-            Side nav
+            <SideNav />
           </Grid>
         </Hidden>
-        <Grid item xs={12} md={6} className={classes.children}>
-          Main Content
+        <Grid item xs={12} md={6} className={classes.post}>
+          {/* <Grid container item justify='center'>
+            <Grid
+              container
+              xs={11}
+              sm={7}
+              md={11}
+              item
+              className={classes.posts}> */}
+          <Posts />
+          {/* </Grid>
+          </Grid> */}
         </Grid>
         <Hidden smDown>
           <Grid item md={3} className={classes.onlineUsers}>
