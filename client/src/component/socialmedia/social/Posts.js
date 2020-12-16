f99ff7f0b738d827ae954a2a97e271d496f8e4b1
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-16 16:44:08 +0530

    received posts from social dashboard component

--- client/src/component/socialmedia/social/Posts.js
+++ client/src/component/socialmedia/social/Posts.js
@@ -1,32 +1,33 @@
-import React from "react";
-import { Grid, makeStyles } from "@material-ui/core";
-import PostItem from "./PostItem";
+import React from 'react';
+import { Grid, makeStyles } from '@material-ui/core';
+import PostItem from './PostItem';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    // paddingLeft: 15
-    // border: "2px solid #eee"
-  }
+	container: {
+		// paddingLeft: 15
+		// border: "2px solid #eee"
+	},
 }));
 
-const Posts = () => {
-  const classes = useStyles();
-  return (
-    <Grid container item justify='center'>
-      <Grid
-        container
-        item
-        xs={11}
-        sm={7}
-        md={11}
-        direction='column'
-        className={classes.container}>
-        <PostItem />
-        <PostItem />
-        <PostItem />
-      </Grid>
-    </Grid>
-  );
+const Posts = ({ posts }) => {
+	const classes = useStyles();
+
+	return (
+		<Grid container item justify='center'>
+			<Grid
+				container
+				item
+				xs={11}
+				sm={7}
+				md={11}
+				direction='column'
+				className={classes.container}>
+				{posts.map((post) => (
+					<PostItem post={post} />
+				))}
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Posts;
