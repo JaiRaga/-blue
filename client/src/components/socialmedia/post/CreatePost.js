d6b37ab463b3046d9d868ddc857e0ee3717317a6
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-31 06:34:57 +0530

    trying to make it reusable

--- client/src/components/socialmedia/post/CreatePost.js
+++ client/src/components/socialmedia/post/CreatePost.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, makeStyles } from "@material-ui/core"
+import { useDispatch } from "react-redux"
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
@@ -7,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
+import { createPost, updatePost } from '../../../redux/actions/social';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -29,10 +31,24 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
-const CreatePost = ({ posttext, handlePostClose }) => {
+// Component for Creating and Editing Post.
+const CreatePost = ({ postId = null, posttext = null, handlePostClose = null }) => {
     const classes = useStyles()
-    console.log(posttext)
+    const dispatch = useDispatch()
+    // console.log(posttext)
+
+    // post text 
+    const [text, setText] = useState('')
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        !handlePostClose ? dispatch(updatePost(postId, text)) : dispatch(createPost(text))
+        return handlePostClose(true)
+
+    }
+
     return (
         <Grid container item xs={10} sm={12} justify="center" alignItems="center">
             <Paper elevation={24} component="form" className={classes.root}>
@@ -48,12 +64,14 @@ const CreatePost = ({ posttext, handlePostClose }) => {
                     autoComplete
                     autoFocus
                     multiline
+                    onChange={handleChange}
+                    value={text}
                 />
-                <IconButton color="primary" className={classes.iconButton} aria-label="submit" onClick={() => handlePostClose(true)}>
+                <IconButton color="primary" className={classes.iconButton} aria-label="submit" onClick={() => handleSubmit()}>
                     <CheckIcon />
                 </IconButton>
                 <Divider className={classes.divider} orientation="vertical" />
-                <IconButton color="secondary" className={classes.iconButton} aria-label="close" onClick={() => handlePostClose(true)}>
+                <IconButton color="secondary" className={classes.iconButton} aria-label="close" onClick={() => handleSubmit()}>
                     <CloseIcon />
                 </IconButton>
             </Paper>
